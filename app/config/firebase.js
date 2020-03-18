16aa8000184b37f97835538d0411b0585063c022
NielsMoens <nielmoen@student.arteveldehs.be>
2020-03-18 12:19:34 +0100

    add config

---
+++ app/config/firebase.js
@@ -0,0 +1,12 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCA_u-lGI9zlw8oVYIPIfZ0wKPTBAZOzmM",
+    authDomain: "iotlabo3.firebaseapp.com",
+    databaseURL: "https://iotlabo3.firebaseio.com",
+    projectId: "iotlabo3",
+    storageBucket: "iotlabo3.appspot.com",
+    messagingSenderId: "172396452367",
+    appId: "1:172396452367:web:273a5ec3c1fdbf8784e17a"
+  };
+
